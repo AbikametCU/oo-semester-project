bcf093a9141c91d1c61bd06879a7a58cd2d5ec1e
“Abikamet <aban7446@colorado.edu>
2020-12-07 22:45:09 -0700

    added basic info fragment information

--- app/src/main/java/com/example/norona/MainActivity.java
+++ app/src/main/java/com/example/norona/MainActivity.java
@@ -14,8 +14,6 @@ import org.tensorflow.lite.examples.classification.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
